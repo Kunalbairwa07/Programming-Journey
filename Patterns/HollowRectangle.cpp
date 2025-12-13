44633d4c82430d4e9e21ea2fd16d68f0617ab82b
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-13 23:08:04 +0530

    Hollow Rectangle

--- Patterns/HollowRectangle.cpp
+++ Patterns/HollowRectangle.cpp
@@ -8,10 +8,10 @@ int main(){
         cout<<"*";
         for(int j=1; j<=n-1; j++){
             if(i==1 || i==n){
-                cout<<"*";
+                cout<<"*"; // for star filling 
             }
             else{
-                cout<<" ";
+                cout<<" "; // For White spaces
             }
         }
         cout<<"*"<<endl;
