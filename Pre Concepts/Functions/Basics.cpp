5483b4883a32eef7784eab4079011dafa6dba198
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-18 12:35:28 +0530

    new

---
+++ Pre Concepts/Functions/Basics.cpp
@@ -0,0 +1,20 @@
+/*
+THE FUNCTIONS
+returnType fName(){
+//do some work
+return some value;
+}
+
+fName(); // Function Call
+*/
+#include <iostream>
+using namespace std;
+
+void SayHello(){
+    cout<<"Hello User.";
+}
+
+int main(){
+    SayHello();
+    return 0;
+}
