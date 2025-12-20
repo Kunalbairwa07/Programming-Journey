039de8d060d130c90ddbe22a8f36aba6e9fc1e8f
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-20 22:30:46 +0530

    Product of 2 no. by Functions

---
+++ Pre Concepts/Functions/Product_by_Function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+float product(float a, float b){
+    float product = a*b;
+    return product;
+}
+int main(){
+    product(2.5,2);
+    cout<<"Product = "<<product;
+    return 0;
+}
+/*
+This code is for getting produc of 2 numbers by function.
+*/
