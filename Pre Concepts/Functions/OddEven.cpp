2366be74ff997235e8b2080f0a2be528eda278ca
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-20 22:54:43 +0530

    Odd Even by Function

---
+++ Pre Concepts/Functions/OddEven.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool oddEven(int n)
+{
+    if (n % 2 == 0)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    int n;
+    cout << "Enter Your Number = ";
+    cin >> n;
+    bool result = oddEven(n);
+    if (result == true) // = & == both change the entire output.
+    {
+        cout << "The number is even";
+    }
+    else
+    {
+        cout << "The number is odd";
+    }
+    return 0;
+}
+
+//other alternate of if else (n % 2 == 0) ? "even" : "odd";
