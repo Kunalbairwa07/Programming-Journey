17f355db5eaef6f598247d61feb34f6a77a555b9
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-27 19:43:40 +0530

    Primes in Range

---
+++ Pre Concepts/Functions/Primes_in_Range.cpp
@@ -0,0 +1,31 @@
+// to print the number betweem 2 to n (Primes).
+#include <iostream>
+using namespace std;
+int n;
+
+bool isprime(int n){
+    if(n<=1){
+        return false;
+    }
+    for(int i=2; i*i<=n; i++){
+        if (n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void allPrimes(int n){
+    for (int i=1; i<=n; i++){
+        if(isprime(i)){
+            cout<<i<<" ";
+        }
+    }
+}
+int main (){
+    cout<<"Enter the value = ";
+    cin>>n;
+    cout<<"Prime numbers up to "<< n << " are: ";
+    allPrimes(n);
+    return 0;
+}
