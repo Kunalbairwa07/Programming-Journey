354a1d85f80bd8b8dc9795bc9f087fcce1b4c0e6
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-28 21:28:33 +0530

    Primes in Range

--- Pre Concepts/Functions/Primes_in_Range.cpp
+++ Pre Concepts/Functions/Primes_in_Range.cpp
@@ -3,29 +3,37 @@
 using namespace std;
 int n;
 
-bool isprime(int n){
-    if(n<=1){
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
         return false;
     }
-    for(int i=2; i*i<=n; i++){
-        if (n%i==0){
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             return false;
         }
     }
     return true;
 }
 
-void allPrimes(int n){
-    for (int i=1; i<=n; i++){
-        if(isprime(i)){
-            cout<<i<<" ";
+void allPrimes(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << " ";
         }
     }
 }
-int main (){
-    cout<<"Enter the value = ";
-    cin>>n;
-    cout<<"Prime numbers up to "<< n << " are: ";
+int main()
+{
+    cout << "Enter the value = ";
+    cin >> n;
+    cout << "Prime numbers up to " << n << " are: ";
     allPrimes(n);
     return 0;
 }
