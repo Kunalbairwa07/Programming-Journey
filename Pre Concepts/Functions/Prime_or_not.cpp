b0764015981ef4147c3754215f64107aaa08ebbf
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-30 17:51:27 +0530

    Prime Or Not using Function

--- Pre Concepts/Functions/Prime_or_not.cpp
+++ Pre Concepts/Functions/Prime_or_not.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
+bool isPrime(int n)
+{
     if (n <= 1)
         return false;
 
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i * i <= n; i++)
+    {
         if (n % i == 0)
             return false;
     }
@@ -14,14 +16,18 @@ bool isPrime(int n) {
     return true;
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter number = ";
     cin >> n;
 
-    if (isPrime(n)) {
+    if (isPrime(n))
+    {
         cout << "The number " << n << " is Prime." << endl;
-    } else {
+    }
+    else
+    {
         cout << "The number " << n << " is not Prime." << endl;
     }
 
