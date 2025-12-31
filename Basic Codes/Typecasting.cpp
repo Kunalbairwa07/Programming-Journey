25610315af3f095b94766b610cf951086234c0dd
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2025-12-31 18:05:44 +0530

    Type Casting

--- Basic Codes/Typecasting.cpp
+++ Basic Codes/Typecasting.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<((bool)3+2)<<endl; // operation with any number wit bool results 1 except with 0.
-    cout<<(23.5+2+'A');// bool < char < int < float < double.(Priority)
+int main()
+{
+    cout << ((bool)3 + 2) << endl; // operation with any number wit bool results 1 except with 0.
+    cout << (23.5 + 2 + 'A');      // bool < char < int < float < double.(Priority)
     return 0;
 }
