8d48e3024f9f318078a40dfea1eb4df7e6619307
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-01 20:26:18 +0530

    Basics of Pointers

---
+++ Pre Concepts/Pointers/Basic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    cout << "The location of variable a = " << &a << endl;
+    return 0;
+}
+/*
+Pointers = A special variable that stores address of another variable.
+*/
