586946aa927e9f5cc9da47ab2bedcb90145f21c0
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-02 12:23:16 +0530

    Null Pointer

---
+++ Pre Concepts/Pointers/Null_Pointer.cpp
@@ -0,0 +1,11 @@
+/*
+NULL Pointer = We assign null value to a pointer to show that it doesn't point to any Location.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *ptr = NULL;
+    cout << ptr;
+    return 0;
+}
