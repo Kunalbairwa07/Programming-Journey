e438a1d119d62776d9cc7e1cd855c3516eccbc33
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-03 19:48:36 +0530

    Starting the Array

---
+++ Data Structures & Algorithms/Arrays/Baiscs.cpp
@@ -0,0 +1,8 @@
+// Arrays :- Linear collection of same type of elements are stored together in contiguous memory spaces.
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
