8ec50994009c51e9b522355db69af0c6c1fb0911
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-05 20:51:35 +0530

    Basics of arrays

--- Data Structures & Algorithms/Arrays/Baiscs.cpp
+++ Data Structures & Algorithms/Arrays/Baiscs.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 int main()
 {
-    int marks [50];
-    cout<<marks;
+    int marks[50];
+    int marks2[50] = {1, 2, 3, 4, 5, 6, 7};
+    int marks3[] = {1, 2, 3};
+    cout << marks[7] << endl;
+    cout << marks2[6] << endl;
+    cout << marks3[2] << endl;
     return 0;
 }
