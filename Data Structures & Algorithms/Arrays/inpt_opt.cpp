e7503c7740424a11e37c1ce2e2d9b763c7eea068
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-07 23:49:52 +0530

    Inputs and outputs in an Array

---
+++ Data Structures & Algorithms/Arrays/inpt_opt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {7, 5, 9, 4, 1, 2, 5, 7, 4, 1};
+    int n = sizeof(arr) / sizeof(int); // this will give the length of the array.
+
+    // for (int i = 0; i < n; i++)
+    // {                                 (for taking input)
+    //     cin >> arr[i];
+    // }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
