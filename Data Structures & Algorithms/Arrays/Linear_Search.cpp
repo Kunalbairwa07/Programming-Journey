d3f13fffff502bcb4adea7478b25751076576f0f
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-17 23:56:58 +0530

    Linear Search

--- Data Structures & Algorithms/Arrays/Linear_Search.cpp
+++ Data Structures & Algorithms/Arrays/Linear_Search.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void joker()
+int LinearSearch(int arr[], int n, int key)
 {
-    cout << "Hey joker" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main()
 {
-    joker();
+    int inp;
+    cout << "Enter the Element you have to find in array = ";
+    cin >> inp;
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int n = sizeof(arr) / sizeof(int);
+    int index = LinearSearch(arr, n, inp);
+
+    if (index == -1)
+        cout << "Key not found" << endl;
+    else
+        cout << "The index for Key is = " << index << endl;
     return 0;
 }
