993472615fe2b7f9ad3f6f398083a1efafc45421
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-18 19:34:31 +0530

    Linear Search in Array

--- Data Structures & Algorithms/Arrays/Linear_Search.cpp
+++ Data Structures & Algorithms/Arrays/Linear_Search.cpp
@@ -19,7 +19,7 @@ int main()
     cout << "Enter the Element you have to find in array = ";
     cin >> inp;
     int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
-    int n = sizeof(arr) / sizeof(int);
+    int n = sizeof(arr) / sizeof(int); //For the size of Array.
     int index = LinearSearch(arr, n, inp);
 
     if (index == -1)
@@ -28,3 +28,8 @@ int main()
         cout << "The index for Key is = " << index << endl;
     return 0;
 }
+/*
+Time complexity = Relation b/W input size & no. of Operations.
+Linear Time Complexity O(n).
+It can perform on Both Sorted or Unsorted Array.
+*/
