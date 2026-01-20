d244b4c9be49820c42b486675ba36284af94e995
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-20 22:41:07 +0530

    Reverse Of An Array

--- Data Structures & Algorithms/Arrays/Reverse_array.cpp
+++ Data Structures & Algorithms/Arrays/Reverse_array.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Hii";
+
+int main()
+{
+    int inp;
+    cout << "Enter the Size for the array = ";
+    cin >> inp;
+    int arr[100];
+    int size = sizeof(arr) / sizeof(int);
+
+    cout << "Enter Elements \n";
+    for (int i = 0; i < inp; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The Reverse array is = ";
+    for (int i = inp-1; i >= 0; i--) // This is the most important Logic in this.
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
