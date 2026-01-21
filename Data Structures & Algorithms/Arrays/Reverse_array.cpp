d97a620accefbd4d000c14d64dc14eadcc4db819
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-01-21 23:49:31 +0530

    Reverse of an Array

--- Data Structures & Algorithms/Arrays/Reverse_array.cpp
+++ Data Structures & Algorithms/Arrays/Reverse_array.cpp
@@ -16,10 +16,43 @@ int main()
     }
 
     cout << "The Reverse array is = ";
-    for (int i = inp-1; i >= 0; i--) // This is the most important Logic in this.
+    for (int i = inp - 1; i >= 0; i--) // This is the most important Logic in this.
     {
         cout << arr[i] << " ";
     }
 
     return 0;
 }
+
+/*
+There is so many method to solve a question (Many approaches)
+Reverse of Array can be solve by majorly two method - One is with extra space like copy array method & Another one is to solve by taking two pointer as a start or end & the swap it.
+and last but not the least one is to solve by PreBuilt "Swap()" Function.
+*/
+/*
+Two pointer or Variable 
+int start = 0, end = size - 1;
+
+while (start < end)
+{
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+}
+    printArr(arr,size)
+*/
+
+/*
+Using swap()function
+int start = 0, end = size - 1;
+
+while (start < end)
+{
+    swap(arr[start],arr[end])
+    start++;
+    end--;
+}
+    printArr(arr,size)
+*/
