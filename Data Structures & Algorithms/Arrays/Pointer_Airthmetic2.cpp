45c47095b6899d7af2cf8deb2e3d8f62d1b93a45
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-02-08 00:03:31 +0530

    Pointer Airthmetic 2

--- Data Structures & Algorithms/Arrays/Pointer_Airthmetic2.cpp
+++ Data Structures & Algorithms/Arrays/Pointer_Airthmetic2.cpp
@@ -1,22 +1,30 @@
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num = 5;
-//     int *ptr = &num;
-//     cout << ptr << endl;
-//     cout << ptr + 5; // this will increment the pointer address with 2.
-//     return 0;
-// }
-
+// Pointer Airthmetic 2.
 #include <iostream>
 using namespace std;
-void printArr(int *ptr, int n){
-
-}
-int main(){
-    int arr[]={1,2,3,4,5};
-    int n = sizeof(arr)/sizeof(int);
-    printArr(ptr,n)
+int main()
+{
+    int num = 5;
+    int *ptr = &num;
+    cout << ptr << endl;
+    cout << ptr + 5; // this will increment the pointer address with 2.
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+void printArr(int *ptr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << *ptr + i << "\n";
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    printArr(arr, n);
+    return 0;
+}
+*/
