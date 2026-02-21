23e6a8431b0b98e06871aa20b18b8d050f583f99
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-02-21 23:49:09 +0530

    new

--- Data Structures & Algorithms/Array_Qs/new.cpp
+++ Data Structures & Algorithms/Array_Qs/new.cpp
@@ -6,7 +6,8 @@ int main()
     cout << "didnt call."<<endl;
     cout<<"Halat or bigad gye"<<endl;
     cout<<"Ab jake chije smbhali h"<<endl;
-    cout<<"kl se sb bdl dunga";
-    cout<<"Sunanda Sharma"
+    cout<<"kl se sb bdl dunga"<<endl;
+    cout<<"Sunanda Sharma"<<endl;
+    cout<<"When i Start";
     return 0;
 }
