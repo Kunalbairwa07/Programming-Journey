4aa939c76e4bd1d407c895e59a4f892bd4fbc295
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2026-03-06 23:49:01 +0530

    MaxSubArray

---
+++ Data Structures & Algorithms/Array_Qs/MaxSubArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void MaxSubArray(int *arr, int n){
+    int sum=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<arr[j]<<" ";
+        }
+    }
+}
+
+int main(){
+    int arr[]={2,-3,6,-5,4,2};
+    int n = sizeof(arr) / sizeof(int);
+    MaxSubArray(arr, n);
+    return 0;
+
+}
